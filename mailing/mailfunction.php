cbc45b5fe2c0c4e83791417265ebc75fa6aa8b05
Markus Brandstetter <markus.brandstetter@it.htlhl.at>
2023-11-04 14:00:26 +0100

    design

--- mailing/mailfunction.php
+++ mailing/mailfunction.php
@@ -6,46 +6,50 @@ use PHPMailer\PHPMailer\SMTP;
 require('./vendor/autoload.php');
 require 'mailingvariables.php';
 
-function mailfunction($mail_reciever_email, $mail_reciever_name, $mail_msg, $attachment = false){
+function mailfunction($mail_reciever_email, $mail_reciever_name, $mail_msg, $attachment = false)
+{
 
-    $mail = new PHPMailer();
-    $mail->isSMTP();
+    try {
+        $mail = new PHPMailer();
+        $mail->isSMTP();
 
-    //$mail->SMTPDebug = SMTP::DEBUG_SERVER;
+        //$mail->SMTPDebug = SMTP::DEBUG_SERVER;
 
-    $mail->Host = $GLOBALS['mail_host'];
+        $mail->Host = $GLOBALS['mail_host'];
 
-    $mail->Port = $GLOBALS['mail_port'];
+        $mail->Port = $GLOBALS['mail_port'];
 
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
 
-    $mail->SMTPAuth = true;
+        $mail->SMTPAuth = true;
 
-    $mail->Username = $GLOBALS['mail_sender_email'];
+        $mail->Username = $GLOBALS['mail_sender_email'];
 
-    $mail->Password = $GLOBALS['mail_sender_password'];
+        $mail->Password = $GLOBALS['mail_sender_password'];
 
-    $mail->setFrom($GLOBALS['mail_sender_email'], $GLOBALS['mail_sender_name']);
+        $mail->setFrom($GLOBALS['mail_sender_email'], $GLOBALS['mail_sender_name']);
 
-    $mail->addAddress($mail_reciever_email, $mail_reciever_name);
+        $mail->addAddress($mail_reciever_email, $mail_reciever_name);
 
-    $mail->Subject = 'Someone Contacted You!';
+        $mail->Subject = 'Someone Contacted You!';
 
-    $mail->isHTML($isHtml = true );
+        $mail->isHTML($isHtml = true);
 
-    $mail->msgHTML($mail_msg);
+        $mail->msgHTML($mail_msg);
 
 
-    if($attachment !== false){
-        $mail->AddAttachment($attachment);
-    }
-    
-    $mail->AltBody = 'This is a plain-text message body';
- 
-    if (!$mail->send()) {
-        return false;
-    } else {
-        return true;
+        if ($attachment !== false) {
+            $mail->AddAttachment($attachment);
+        }
+
+        $mail->AltBody = 'This is a plain-text message body';
+
+        // Send the email
+        $mail->send();
+        return true; 
+    } catch (Exception $e) {    
+        echo 'Email could not be sent. Error: ' . $mail->ErrorInfo;
+        return false; 
     }
 }
 
--- mailing/mailingvariables.php
+++ mailing/mailingvariables.php
@@ -1,7 +1,7 @@
 <?php
     $mail_host = "smtp.gmail.com";
     $mail_port = "587";
-    $mail_sender_email = ""; //sender
-    $mail_sender_password = ""; //sender
+    $mail_sender_email = "bwebcreations@gmail.com"; //sender
+    $mail_sender_password = "jFwFeJN6j8rpNWcm"; //sender
     $mail_sender_name = "Website Form";
 ?>
